03313f9f7447a9616bda2f00cb224b782c109e2d
Moumita Sen <52632685+smoumita19@users.noreply.github.com>
2022-11-08 18:05:06 +0530

    Update LeadersInAnArray.java

--- LeadersInAnArray/LeadersInAnArray.java
+++ LeadersInAnArray/LeadersInAnArray.java
@@ -4,43 +4,31 @@ class LeadersInAnArray {
     public static void main(String[] args) {
 
         int[] arr = {16,17,4,3,5,2};
-        ArrayList<Integer> resArr = new ArrayList<Integer>();
-        int res = -1;
+        ArrayList<Integer> newArr = new ArrayList<Integer>();
         int j = 0;
-        for(int i=0; i< arr.length; i++) {
+        int res = 0;
+        for(int i=0; i<arr.length;i++) {
             j = i+1;
-            while(j<arr.length){
-
-
-
+            if(i == (arr.length-1)){
+                newArr.add(arr[i]);
+            }
+            while(j<arr.length) {
                 if(arr[i] > arr[j]) {
                     res = arr[i];
-                    j++;
-
-
-                }
-                else {
+                } else {
                     break;
                 }
-
-                if(j == arr.length-1) {
-                    System.out.println(res);
-                    resArr.add(res);
+                
+                if(j == (arr.length-1)) {
+                    newArr.add(res);
                 }
-
+                j++;
+                
             }
-
-            if(i == (arr.length-1)){
-                resArr.add(arr[i]);
-            }
-
-
-
-
-
+            
         }
-
-        System.out.println(resArr);
+        
+        return newArr;
 
     }
 
