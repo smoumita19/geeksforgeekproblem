fe50f5715c6195c2dbfab5f5839eb3b7839163f0
Moumita Sen <52632685+smoumita19@users.noreply.github.com>
2022-11-09 18:53:37 +0530

    Update LeadersInAnArray.java

--- LeadersInAnArray/LeadersInAnArray.java
+++ LeadersInAnArray/LeadersInAnArray.java
@@ -1,35 +1,18 @@
-import java.util.*;
-class LeadersInAnArray {
-
-    public static void main(String[] args) {
-
-        int[] arr = {16,17,4,3,5,2};
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
         ArrayList<Integer> newArr = new ArrayList<Integer>();
-        int j = 0;
-        int res = 0;
-        for(int i=0; i<arr.length;i++) {
-            j = i+1;
-            if(i == (arr.length-1)){
-                newArr.add(arr[i]);
+        int res = arr[arr.length-1];
+        for(int i=arr.length-1; i>=0; i--){
+            if(arr[i] >= res){
+                res = arr[i];
+                newArr.add(res);
             }
-            while(j<arr.length) {
-                if(arr[i] > arr[j]) {
-                    res = arr[i];
-                } else {
-                    break;
-                }
-                
-                if(j == (arr.length-1)) {
-                    newArr.add(res);
-                }
-                j++;
-                
-            }
-            
         }
         
+        Collections.reverse(newArr);
+        
         return newArr;
-
     }
-
 }
