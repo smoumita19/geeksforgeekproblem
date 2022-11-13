cf7278733dd4cae1f6e48308234e015e1e59efcf
Moumita Sen <52632685+smoumita19@users.noreply.github.com>
2022-11-13 10:39:45 +0530

    Update EquilibriumPoint.java

--- EquilibriumPoint/EquilibriumPoint.java
+++ EquilibriumPoint/EquilibriumPoint.java
@@ -1,44 +1,28 @@
-class EquilibriumPoint {
-    public static void main(String[] args) {
-        int[] arr = {1,2,1,5,1,1,2,0,0};
-        //int j =arr.length-1;
+class Solution {
 
-
-         long sum=0;
-
-       for(int i=0;i<n;i++) {
-
-            sum=sum+arr[i];
-
-       }
-
-       long left=0,right=sum-arr[0];
-
-       if(left==right) return 1;
-
-       for(int i=1;i<n;i++) {
-
-       left=left+arr[i-1];
-
-       right=right-arr[i];
-
-       if(left==right) {
-
-       return i+1;
-
-       }
-
-
-
-
-
-       }
-
-       return -1;
-
+    int equilibriumPoint(long long a[], int n) {
+    
+        // Your code here
+        long sum =0;
+        
+        for(int i=0; i<n;i++) {
+            sum += a[i];
         }
-
-        //return -1;
-
+        
+        long sumleft= 0, sumright = sum - a[0];
+        if(sumleft == sumright){
+            return 1;
+        }
+        
+        for(int i=1; i<n;i++) {
+            sumleft = sumleft+a[i-1];
+            sumright = sumright-a[i];
+            if(sumleft==sumright) {
+                return i+1;
+            }
+        }
+        
+        return -1;
     }
+
 }
