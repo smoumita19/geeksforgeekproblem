7dce0efdb1fa5562a05d7a1320d31c043ed95753
Moumita Sen <52632685+smoumita19@users.noreply.github.com>
2022-11-15 12:32:48 +0530

    Update FindDuplicatesInAnArray.java

--- FindDuplicatesInAnArray/FindDuplicatesInAnArray.java
+++ FindDuplicatesInAnArray/FindDuplicatesInAnArray.java
@@ -1,36 +1,32 @@
-import java.util.*;
-
-class FindDuplicatesInAnArray {
-    public static void main(String[] args) {
-        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
-
-        int[] arr = {1,2,2,3,1};
-        for(int i=0; i<arr.length;i++) {
+class Solution {
+    public static ArrayList<Integer> duplicates(int arr[], int n) {
+        // code here
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        ArrayList<Integer> arrlist = new ArrayList<Integer>();
+        for(int i=0; i<n; i++){
             map.put(arr[i],0);
         }
-
-        for(int i=0; i<arr.length;i++) {
-            int c =0;
+        
+        for(int i=0; i<n; i++) {
+            int c = 0;
             if(map.containsKey(arr[i])) {
                 c = map.get(arr[i]);
-
             }
-            c = c+1;
+            c= c+1;
             map.put(arr[i],c);
-              
         }
-
-
-
-        System.out.println(map);
-        ArrayList<Integer> arrlist = new ArrayList<Integer>();
-
-        for (Integer key : map.keySet()) {
+        
+        for(Integer key : map.keySet()) {
             if(map.get(key) > 1) {
                 arrlist.add(key);
             }
         }
-
-        System.out.println(arrlist);
+        
+        if(arrlist.size() <=0 ) {
+            arrlist.add(-1);
+        } 
+        Collections.sort(arrlist);  
+        return arrlist;
+        
     }
 }
